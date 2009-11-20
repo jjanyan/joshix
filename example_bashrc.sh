6c582b4a0bddbbd20b64c84ffbbe0bc884ba3393
Joshua Anyan <josh.anyan@announcemedia.com>
2009-11-19 22:10:30 -0600

    example bashrc file to inclue joshix with, the variable is important

---
+++ example_bashrc.sh
@@ -0,0 +1,4 @@
+# PUT THIS WHERE YOU WOULD NORMALLY HAVE YOUR .bashrc
+# MODIFY joshix_path TO POINT TO THE FOLDER (WITHOUT THE TRAILING SLASH!)
+joshix_path=~/scripts/joshix
+source $joshix_path"/bash_include.sh"
