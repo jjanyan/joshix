378813e19371f835e8b1d8819c2f41e6a15a0cee
Joshua Anyan <josh.anyan@announcemedia.com>
2009-12-01 20:44:12 -0600

    updated example bashrc to include the variable for one of the reset functions

--- example_bashrc.sh
+++ example_bashrc.sh
@@ -1,4 +1,5 @@
 # PUT THIS WHERE YOU WOULD NORMALLY HAVE YOUR .bashrc
 # MODIFY joshix_path TO POINT TO THE FOLDER (WITHOUT THE TRAILING SLASH!)
 joshix_path=~/scripts/joshix
+basedir=/var/www/html/yourfolderhere
 source $joshix_path"/bash_include.sh"
