45c09caa49bbef7ce78802068eca56541f9859c2
Joshua Anyan <github@anyan.org>
2011-11-22 22:38:54 -0600

    moved some lines around and added a few from my .bashrc file

--- general/general.sh
+++ general/general.sh
@@ -3,3 +3,19 @@ alias jdestroy='rm -rf '
 GREP_OPTIONS="--exclude-dir=\.svn"
 export GREP_OPTIONS
 export GREP_OPTIONS="-I --color --exclude=\*.svn\*"
+
+
+############################################################################
+## SET BASH COMMAND VARIABLES
+############################################################################
+set -o vi
+bind -m vi-insert "\C-l":clear-screen
+export HISTFILESIZE=3000 # the bash history should save 3000 commands
+export HISTCONTROL=ignoreboth # don't put duplicate lines in the history. & ignore things that start with a space
+############################################################################
+
+############################################################################
+## SET PATHS
+############################################################################
+export PATH=$PATH:~/bin
+############################################################################
