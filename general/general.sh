0a6b8437d780dd13768174600e598bf696268f0a
Joshua Anyan <github@anyan.org>
2015-01-10 22:03:21 -0600

    improved json script and grep options

--- general/general.sh
+++ general/general.sh
@@ -1,6 +1,6 @@
 GREP_OPTIONS="--exclude-dir=\.svn"
 #export GREP_OPTIONS
-export GREP_OPTIONS="-I --color --exclude=\*.svn\*"
+export GREP_OPTIONS="-s -I --color --exclude=\*.svn\*"
 
 
 ############################################################################
