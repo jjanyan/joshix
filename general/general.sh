192dc3f597b2397bf677053df8b5b6bcb92aaa98
Joshua Anyan <github@anyan.org>
2015-10-13 00:23:49 -0500

    twaeks

--- general/general.sh
+++ general/general.sh
@@ -22,9 +22,12 @@ export PATH=$PATH:~/.bin:~/.bin_local
 complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
 alias gs='git status'
 alias gd="git diff $1"
+alias gdw="git diff --word-diff $1"
 alias ga="git add -A $1"
 alias gc="git commit"
 alias gp="git pull"
+alias gpr="git pull --rebase"
 alias gl="git log --oneline --decorate"
-alias gls="git log --oneline --decorate --stat"
+alias gls="git log --pretty=format:'%C(dim white)%h %ad%Creset %C(normal)%s%Creset | %C(yellow)%an%Creset' --stat"
+#%C(white)white%Creset
 alias gflush='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
